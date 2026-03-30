f671271364b0d81a89dfe0a3830eda043f344582
Rohan-Shridhar <rohansm668@gmail.com>
2026-03-30 23:52:31 +0530

    Added icons for download and github, added forks

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,8 +1,13 @@
 const Menu = ({ downloadImage }) => {
     return (
         <div className="menu-cont">
-            <button className="menu-btn" onClick={downloadImage}>Download</button>
-            <button className="menu-btn" onClick={() => window.location.href = "https://github.com/Rohan-Shridhar/gridcraft"}>GitHub</button>
+            <button className="menu-btn" onClick={downloadImage}><i className="fa-regular fa-file-image"></i></button>
+            <button className="menu-btn" onClick={() => window.location.href = "https://github.com/Rohan-Shridhar/gridcraft"}>
+                <i className="fab fa-github"></i>
+            </button>
+            <button className="menu-btn" onClick={() => window.location.href = "https://github.com/Rohan-Shridhar/gridcraft/forks"}>
+                <i className="fa-solid fa-code-fork"></i>
+            </button>
         </div>
     );
 }
